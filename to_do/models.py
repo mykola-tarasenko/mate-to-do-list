24058fdc96ca0c6342fadbd2a50932ceaa7238ed
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-02-01 01:33:05 +0200

    Fix ordering in task model

--- to_do/models.py
+++ to_do/models.py
@@ -16,7 +16,7 @@ class Task(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
 
     class Meta:
-        ordering = ("is_done",)
+        ordering = ("is_done", "-created_at",)
 
     def __str__(self):
         return self.content
